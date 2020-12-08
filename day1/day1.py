7afa9625b12b6b4f1d8fc3b8b73fad2b05755299
Boudewijn Swen <boudewijn.swen@ogd.nl>
2020-12-08 13:04:16 +0100

    day 1 initial commit

---
+++ day1/day1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+with open("./day1_list", "r") as stuff:
+    list1337 = []
+    for item in stuff:
+        list1337.append(int(item.strip()))
+    for x in list1337:
+        for y in list1337:  
+            if x + y == 2020:
+                print(x, y)
